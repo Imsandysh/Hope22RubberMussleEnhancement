be75409ccef96eb20afaa1bffb24dfd295190060
Imsandysh <033561485Lahan>
2022-02-06 04:51:54 -0500

    finsl code for emg sensor and relay combination

--- arduino_lib/SensorRelay.js
+++ arduino_lib/SensorRelay.js
@@ -2,21 +2,26 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
-  var sensor = new five.Sensor("A0")
+  var sensor = new five.Sensor({
+    pin: "A0",
+    freq : 1000
+  });
   var relay = new five.Relay(13);
 
   sensor.on("change", function () {
     console.log(this.value);
     var val = this.value;
-
-    if (val > 100){
+    if (val > 200){
+      relay.close()
+    }
+     else{
       relay.open()
-      }else{
-        relay.close()
+      
+    
       }
+
     });
       this.repl.inject({
-          relay: relay
-     
+          relay: relay  
     })
   });
